1eb0b9b33dab37308b770f23c559dc2d15bf0029
Bruno de Pádua Allegretti Sousa <brunoifspcar@gmail.com>
2025-05-20 21:42:13 -0300

    html do header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,24 @@ export default function Header() {
   return (
     <>
       <header>
+        <h2>Once 11</h2>
+        <nav>
+            <ul>
+                <li>Entrar</li>
+                <li>Produtos</li>
+                <li>Produtos</li>
+                <li>Produtos</li>
+            </ul>
+        </nav>
 
+        <div className='search'>
+            <h2>Busca</h2>
+            <span className="material-symbols-outlined">search</span>
+        </div>
+
+        <button className="cart">
+        <span className="material-symbols-outlined">shopping_cart</span>
+        </button>
       </header>
     </>
   )
