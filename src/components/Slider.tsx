d758f95c56fe60d2210cd1b2fd65a11e9160587b
Iago Carvalho <pkrockep@gmail.com>
2025-05-21 20:14:30 -0300

    Slide Funcional

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -5,6 +5,50 @@ import slide2 from '../assets/img/slide2.png';
 
 export default function Slide() {
 
+  const [count, setCount] = useState(1);
+
+  function Proximo() {
+    setCount(prev => {
+      const next = prev + 1 > 4 ? 1 : prev + 1;
+
+      const nextRadio = document.getElementById("radio" + next) as HTMLInputElement;
+      if (nextRadio) nextRadio.checked = true;
+
+      return next;
+    });  
+  }
+
+  function Anterior() {
+    setCount(prev => {
+      const next = prev - 1 < 1 ? 4 : prev - 1;
+
+      const nextRadio = document.getElementById("radio" + next) as HTMLInputElement;
+      if (nextRadio) nextRadio.checked = true;
+
+      return next;
+    });  
+  }
+
+  
+  useEffect(() => {
+    const firstRadio = document.getElementById("radio1") as HTMLInputElement;
+
+    if(firstRadio) firstRadio.checked = true;
+
+    const interval = setInterval(() => { 
+
+      setCount(prev => {
+        const next = prev + 1 > 4 ? 1 : prev + 1;
+
+        const nextRadio = document.getElementById("radio" + next) as HTMLInputElement;
+        if (nextRadio) nextRadio.checked = true;
+
+        return next;
+      });   
+    }, 5000);
+    return () => clearInterval(interval); // limpa intervalo ao desmontar
+  }, []);
+
   return (
     <section className="section-slide">
       <div className="slider">
@@ -28,8 +72,8 @@ export default function Slide() {
           </div>
 
             <div className="manual-navigation">
-                <button className="anterior" onClick={}><span className="material-symbols-outlined">chevron_left</span></button>
-                <button className="proximo" onClick={}><span className="material-symbols-outlined">chevron_right</span></button>
+                <button className="anterior" onClick={Anterior}><span className="material-symbols-outlined">chevron_left</span></button>
+                <button className="proximo" onClick={Proximo}><span className="material-symbols-outlined">chevron_right</span></button>
             </div>
         </div>
       </div>
