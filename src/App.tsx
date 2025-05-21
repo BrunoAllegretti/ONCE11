eae97f3c2b89b4d0260fd1d72458875047c43618
Leonardo Di Ciommo <leozinho.ciommo@gmail.com>
2025-05-21 18:09:53 -0300

    adicionando no App.tsx e css

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Header from './components/Header';
 import Banner from './components/Banner';
+import Carousel from './components/Carousel';
 
 export default function App() {
   return (
     <>
       <Header />  
       <Banner />  
+      <Carousel />
     </>
   );
 }
