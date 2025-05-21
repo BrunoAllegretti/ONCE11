a00cb40076d2b7a34ee33ec9181e12032e37ce10
Bruno de Pádua Allegretti Sousa <brunoifspcar@gmail.com>
2025-05-20 22:16:39 -0300

    Pasta Assets adicionada

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Header from './components/Header';
+import Banner from './components/Banner';
 
 export default function App() {
   return (
     <>
       <Header />  
+      <Banner />  
     </>
   );
 }
