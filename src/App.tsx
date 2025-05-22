20f01231e7dad7e316bd65954d4d21ac8cf1a533
João Henrique <pkrocket788@gmail.com>
2025-05-22 00:05:12 -0300

    Arrumando erros e estilização

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,22 @@ import Header from './components/Header';
 import Banner from './components/Banner';
 import Carousel from './components/Carousel';
 import Promo from './components/PromoBanner';
-import Slide from './components/Slider'
-import Products2 from './components/Products2';
 import Products from './components/Products';
+import Slide from './components/Slide';
+import Products2 from './components/Products2';
 import EightL from './components/EightLine';
 import Footer from './components/Footer';
+
 
 export default function App() {
   return (
     <>
       <Header />  
-      <Banner />  
-      <Carousel />
+      <Banner /> 
+      <Carousel /> 
       <Promo />
       <Slide />
+      <h2 className="aviso">Aviso o Seletor de Esporte será feito na Sprint 4</h2>
       <Products2 />
       <Products />
       <EightL />
